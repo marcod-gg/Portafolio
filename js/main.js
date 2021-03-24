1ef42797f4abfebf63a2e8b50550bbbd1f3b4ddd
Marco D. González <marcod.gonzalezg@gmail.com>
2021-03-24 18:07:52 -0300

    v1.1 - Adaptado a algunas resoluciones

---
+++ js/main.js
@@ -0,0 +1,93 @@
+AOS.init();
+
+/* let ubicacionPrincipal = window.pageYOffset;
+
+window.addEventListener("scroll", function () {
+    let desplazamientoActual = window.pageYOffset;
+    if (ubicacionPrincipal >= desplazamientoActual) {
+        document.getElementsByTagName("nav")[0].style.top = "0px"
+    } else {
+        document.getElementsByTagName("nav")[0].style.top = "-100px"
+    }
+    ubicacionPrincipal = desplazamientoActual;
+})
+ */
+/* Menu */
+
+let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+let semaforo = true;
+
+document.querySelectorAll(".hamburguer")[0].addEventListener("click", function () {
+    if (semaforo) {
+        document.querySelectorAll(".hamburguer")[0].style.color = "#ffffff"
+        semaforo = false;
+    } else {
+        document.querySelectorAll(".hamburguer")[0].style.color = "#ffffff"
+        semaforo = true;
+    }
+    enlacesHeader.classList.toggle("menudos")
+})
+
+
+
+const typed = new Typed('.typed', {
+    strings: ['Ingeniero en Informática', 
+              'Desarrollador WEB',
+              'TechnoJunior'],
+    typeSpeed: 50,
+    startDelay: 500,
+    backSpeed: 25,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+});
+
+function scrollToTop (duration) {
+    // cancel if already on top
+    if (document.scrollingElement.scrollTop === 0) return;
+
+    const cosParameter = document.scrollingElement.scrollTop / 2;
+    let scrollCount = 0, oldTimestamp = null;
+
+    function step (newTimestamp) {
+        if (oldTimestamp !== null) {
+            // if duration is 0 scrollCount will be Infinity
+            scrollCount += Math.PI * (newTimestamp - oldTimestamp) / duration;
+            if (scrollCount >= Math.PI) return document.scrollingElement.scrollTop = 0;
+            document.scrollingElement.scrollTop = cosParameter + cosParameter * Math.cos(scrollCount);
+        }
+        oldTimestamp = newTimestamp;
+        window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
+}
+/* 
+  Explanation:
+  - pi is the length/end point of the cosinus intervall (see below)
+  - newTimestamp indicates the current time when callbacks queued by requestAnimationFrame begin to fire.
+    (for more information see https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame)
+  - newTimestamp - oldTimestamp equals the delta time
+
+    a * cos (bx + c) + d                        | c translates along the x axis = 0
+  = a * cos (bx) + d                            | d translates along the y axis = 1 -> only positive y values
+  = a * cos (bx) + 1                            | a stretches along the y axis = cosParameter = window.scrollY / 2
+  = cosParameter + cosParameter * (cos bx)  | b stretches along the x axis = scrollCount = Math.PI / (scrollDuration / (newTimestamp - oldTimestamp))
+  = cosParameter + cosParameter * (cos scrollCount * x)
+*/
+
+const links = document.querySelectorAll(".enlaces-header a");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
